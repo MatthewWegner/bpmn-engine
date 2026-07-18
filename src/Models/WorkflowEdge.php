93d3b6f1e3a9f177df1eba818671ff46457c9fef
Matthew Wegner <mwegner@proton.me>
2026-07-18 00:43:34 -0400

    add relationship definitions

--- src/Models/WorkflowEdge.php
+++ src/Models/WorkflowEdge.php
@@ -3,8 +3,17 @@
 namespace MatthewWegner\BpmnEngine\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class WorkflowEdge extends Model
 {
     protected $guarded = [];
+
+    /**
+     * Get the workflow version that owns this edge.
+     */
+    public function version(): BelongsTo
+    {
+        return $this->belongsTo(WorkflowVersion::class, 'workflow_version_id');
+    }
 }
--- src/Models/WorkflowNode.php
+++ src/Models/WorkflowNode.php
@@ -3,8 +3,17 @@
 namespace MatthewWegner\BpmnEngine\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class WorkflowNode extends Model
 {
     protected $guarded = [];
+
+    /**
+     * Get the workflow version that owns this node.
+     */
+    public function version(): BelongsTo
+    {
+        return $this->belongsTo(WorkflowVersion::class, 'workflow_version_id');
+    }
 }
