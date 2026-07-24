97857dd6a47c9ccc3e969e82c35772cf0a0cd0bd
Matthew Wegner <mwegner@proton.me>
2026-07-24 11:44:57 -0400

    remove filament permission generation; shouldn't introduce it here, let it be done at app-level

--- src/BpmnEngineServiceProvider.php
+++ src/BpmnEngineServiceProvider.php
@@ -59,9 +59,6 @@ class BpmnEngineServiceProvider extends ServiceProvider
         }
 
         $this->registerGates();
-
-        // Automate Filament Shield integration if the app uses it
-        $this->registerWithFilamentShield();
     }
 
     public function register()
@@ -87,28 +84,5 @@ class BpmnEngineServiceProvider extends ServiceProvider
         Gate::define('bpmn:suspend-instance', fn ($user = null) => app()->environment('local'));
         Gate::define('bpmn:resume-instance', fn ($user = null) => app()->environment('local'));
         Gate::define('bpmn:halt-instance', fn ($user = null) => app()->environment('local'));
-    }
-
-    protected function registerWithFilamentShield(): void
-    {
-        // Check if Filament Shield's config has been loaded by the host app
-        if ($this->app->has('config') && config()->has('filament-shield')) {
-            
-            // Force the "Custom Permissions" UI tab to stay active
-            config([
-                'filament-shield.shield_resource.tabs.custom_permissions' => true,
-                'filament-shield.entities.custom_permissions' => true,
-            ]);
-
-            // Dynamically merge package's permissions into the app's existing config
-            $currentCustomPermissions = config('filament-shield.custom_permissions', []);
-            
-            config([
-                'filament-shield.custom_permissions' => array_merge($currentCustomPermissions, [
-                    'bpmn:view' => 'View BPMN Workflows',
-                    'bpmn:edit' => 'Edit BPMN Workflows',
-                ]),
-            ]);
-        }
     }
 }
