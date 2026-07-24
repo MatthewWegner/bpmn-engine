be31daf83ed66d0b31f98855fd3a0e15637a39c7
Matthew Wegner <mwegner@proton.me>
2026-07-24 09:43:27 -0400

    correct package routes

--- routes/web.php
+++ routes/web.php
@@ -5,16 +5,16 @@ use MatthewWegner\BpmnEngine\Http\Controllers\WorkflowController;
 use MatthewWegner\BpmnEngine\Http\Controllers\WorkflowInstanceController;
 
 // The main entry point to view and manage workflows
-Route::get('/bpmn/workflows', [WorkflowController::class, 'index'])->name('bpmn.index');
+Route::get('/definitions', [WorkflowController::class, 'index'])->name('bpmn.index');
 
 // Process form submission to create new workflows
-Route::post('/bpmn/workflows', [WorkflowController::class, 'store'])->name('bpmn.store');
+Route::post('/definitions', [WorkflowController::class, 'store'])->name('bpmn.store');
 
 // The design editor canvas
-Route::get('/bpmn/workflows/{definition}/design', [WorkflowController::class, 'design'])->name('bpmn.design');
+Route::get('/definitions/{definition}/design', [WorkflowController::class, 'design'])->name('bpmn.design');
 
 // Instance Tracking & Control Routes
-Route::get('/bpmn/instances', [WorkflowInstanceController::class, 'index'])->name('bpmn.instances.index');
-Route::post('/bpmn/instances/{id}/suspend', [WorkflowInstanceController::class, 'suspend'])->name('bpmn.instances.suspend');
-Route::post('/bpmn/instances/{id}/resume', [WorkflowInstanceController::class, 'resume'])->name('bpmn.instances.resume');
-Route::post('/bpmn/instances/{id}/halt', [WorkflowInstanceController::class, 'halt'])->name('bpmn.instances.halt');
+Route::get('/instances', [WorkflowInstanceController::class, 'index'])->name('bpmn.instances.index');
+Route::post('/instances/{id}/suspend', [WorkflowInstanceController::class, 'suspend'])->name('bpmn.instances.suspend');
+Route::post('/instances/{id}/resume', [WorkflowInstanceController::class, 'resume'])->name('bpmn.instances.resume');
+Route::post('/instances/{id}/halt', [WorkflowInstanceController::class, 'halt'])->name('bpmn.instances.halt');
