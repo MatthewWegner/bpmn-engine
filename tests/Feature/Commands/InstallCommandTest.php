332f9e28222b2529cd0bdb353144e21bf3151eaf
Matthew Wegner <mwegner@proton.me>
2026-07-29 20:27:54 -0400

    fix InstallCommandTest by cleaning migrations after each run

--- tests/Feature/Commands/InstallCommandTest.php
+++ tests/Feature/Commands/InstallCommandTest.php
@@ -1,5 +1,10 @@
 <?php
 
+afterEach(function () {
+    // Clean out the published migrations to prevent class collisions in other tests
+    File::cleanDirectory(database_path('migrations'));
+});
+
 it('runs the install command and offers to migrate', function () {
     $this->artisan('bpmn:install')
         ->expectsConfirmation('Would you like to run the database migrations now?', 'no')
